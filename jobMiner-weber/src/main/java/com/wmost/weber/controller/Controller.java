2e00f4959ebd442f276a6f5f8c2cc50baab56793
chenbintao <chenbintao@kingsoft.com>
2017-05-13 19:17:06 +0800

    jobMiner-weber-jsp 增加学习内容

--- jobMiner-weber/src/main/java/com/wmost/weber/controller/Controller.java
+++ jobMiner-weber/src/main/java/com/wmost/weber/controller/Controller.java
@@ -1,33 +1,59 @@
 package com.wmost.weber.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.wmost.framework.security.IgnoreSecurity;
 
 //注解让spring-MVC能扫描到
 @RestController
 @RequestMapping(value = "/samples ")
 public class Controller {
 	//注解方法为一个请求处理方法
+	@IgnoreSecurity
 	@RequestMapping(value = "/A", method = {RequestMethod.POST,RequestMethod.GET})
 	public void A(){
 		
 	}
 	
 	//注解请求参数,形如：/B?key=value
+	@IgnoreSecurity
 	@RequestMapping(value = "/B", method = {RequestMethod.POST,RequestMethod.GET})
-	public String B(@RequestParam("id") String id){
+	public ModelAndView  B(@RequestParam("id") Long id,RedirectAttributes redirect){
 		System.out.println("收到/samples/B/"+id+"请求...");
 		
-		return "redirect:/C/"+id;
+		redirect.addFlashAttribute("id", id);
+
+		return new ModelAndView("redirect:/samples/C/"+id);
 	}
 	
 	//注解路径变量,形如:/C/value
+	@IgnoreSecurity
 	@RequestMapping(value = "/C/{id}", method = {RequestMethod.POST,RequestMethod.GET})
-	public void C(@PathVariable String id){
+	public ModelAndView C(@PathVariable Long id,RedirectAttributes redirect){
 		
 		System.out.println("收到/samples/C/"+id+"请求...");
+		
+		redirect.addFlashAttribute("id", id);
+		
+		return new ModelAndView("redirect:http://www.baidu.com");
+	}
+	
+	//注解路径变量,形如:/C/value
+	@IgnoreSecurity
+	@RequestMapping(value = "/D", method = {RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView D(HttpServletRequest request, HttpServletResponse response){
+		
+		System.out.println("收到/samples/D/"+request.getContextPath()+"请求...");
+		
+		return new ModelAndView("redirect:http://www.baidu.com");
 	}
 }
